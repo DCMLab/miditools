a24a9df0e05016bf493ce091fc66723b071c15d9
Fabian C. Moss <fabianmoss@gmail.com>
2019-04-24 08:38:54 +0200

    modify speller

--- src/midi2tpclist.sh
+++ src/midi2tpclist.sh
@@ -3,10 +3,11 @@
 input="$1" # get source path from user
 output="$2" # get target path from user
 
-mkdir output
-for f in midifiles/kp/*.mid
+mkdir "$S2"
+
+for f in "$input" #./melisma2003/midifiles/kp/*.mid
 do
-  mftext/mftext "$f" | meter/meter | harmony/harmony | grep TPCNote > "output/$(basename "$f" .mid).csv"
+  ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
 done
 
 if [ -s "output/$f.csv" ]
