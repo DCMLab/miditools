b12856c52c8c0ae57d60ae25c789a9c53b77e89a
Fabian C. Moss <fabianmoss@gmail.com>
2019-04-24 08:45:59 +0200

    update speller

--- src/midi2tpclist.sh
+++ src/midi2tpclist.sh
@@ -3,9 +3,12 @@
 input="$1" # get source path from user
 output="$2" # get target path from user
 
-mkdir "$S2"
+echo "$1"
+echo "$2"
 
-for f in "$input" #./melisma2003/midifiles/kp/*.mid
+mkdir "$2"
+
+for f in $input/*.mid #./melisma2003/midifiles/kp/*.mid
 do
   ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
 done
