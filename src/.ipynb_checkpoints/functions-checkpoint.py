001fc8d30cbca4a28b2614c9ece2247bc837e385
Fabian C. Moss <fabianmoss@gmail.com>
2019-04-24 17:32:30 +0200

    compare spelling

---
+++ src/.ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,77 @@
+import numpy as np
+import mido, re
+from scipy.stats import entropy
+
+def tpc_lof_name(lof_number):
+    """returns (a // 7, a % 7)"""
+    a, b = divmod(lof_number + 1, 7)
+    
+    d = {
+        0: 'F',
+        1 : 'C',
+        2 : 'G', 
+        3 : 'D', 
+        4 : 'A',
+        5 : 'E',
+        6 : 'B'
+    }
+    
+    if a == 0:
+        acc = ''
+    elif a > 0:
+        acc = '#'*abs(a)
+    elif a < 0:
+        acc = 'b'*abs(a)
+    
+    return d[b]+acc
+
+def tpc_counts(df):
+    df = df.copy()
+    # Temperley's encoding has C=2 (http://www.link.cs.cmu.edu/music-analysis/harmony.html). Shift so that C=0
+    df['TPC'] -= 2
+    
+    df['tpc_name'] = df['TPC'].map(tpc_lof_name) # add spelled note names
+    df['pc'] = df['TPC'] * 7 % 12
+    
+    tpc_counts = df.tpc_name.value_counts().reindex([tpc_lof_name(i).replace('##', 'x') for i in range(-15,20)]).fillna(0)
+    
+    return tpc_counts
+
+def midi_on_off(file):
+    """Replaces MIDI note_on events with velocity 0 with MIDI note_off events"""
+    
+    try:
+        midi = mido.MidiFile(file)
+
+        a = re.compile(r"""note_on(?P<type>\s)
+                           channel=(?P<channelno>\d\s)
+                           note=(?P<noteno>\d+\s)
+                           velocity=(?P<velocity>\d+\s)
+                           time=(?P<time>\d+)""", re.X)
+
+        for track in midi.tracks:
+            for i, msg in enumerate(track):
+                if not msg.is_meta:
+                    if re.match(a, str(msg)):
+                        if msg.velocity==0:
+                            offmsg = mido.Message('note_off')
+                            offmsg.channel = msg.channel
+                            offmsg.note = msg.note
+                            offmsg.velocity = msg.velocity
+                            offmsg.time = msg.time
+
+                            track[i] = offmsg
+
+        midi.save(file)
+    except Exception as e:
+        print(e, file)
+    
+def jsd(p, q):
+    """return Jenson-Shannon divergence"""
+    p = np.asarray(p)
+    q = np.asarray(q)
+    # normalize
+    p /= p.sum()
+    q /= q.sum()
+    m = (p + q) / 2
+    return (entropy(p, m) + entropy(q, m)) / 2
--- src/midi2tpclist.sh
+++ src/midi2tpclist.sh
@@ -3,17 +3,15 @@
 input="$1" # get source path from user
 output="$2" # get target path from user
 
-echo "$1"
-echo "$2"
-
 mkdir "$2"
 
-for f in $input/*.mid #./melisma2003/midifiles/kp/*.mid
+for f in $(find $input -name "*.mid")
 do
-  ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
+ ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
 done
 
+# remove empty files (in case something went wrong)
 if [ -s "output/$f.csv" ]
 then
-  rm -f "output$f.csc"
+  rm -f "output$f.csv"
 fi
