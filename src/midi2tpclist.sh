ca23816f0dda5f44aeee1c187bfd4930d58473ec
Fabian C. Moss <fabianmoss@gmail.com>
2019-04-27 20:09:56 +0200

    update parser and parse

--- src/midi2tpclist.sh
+++ src/midi2tpclist.sh
@@ -6,12 +6,20 @@ output="$2" # get target path from user
 mkdir "$2"
 
 for f in $(find $input -name "*.mid")
-do
- ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
-done
 
-# remove empty files (in case something went wrong)
-if [ -s "output/$f.csv" ]
+do
+
+if [ -f "$2/$(basename "$f" ".mid").csv" ]
 then
-  rm -f "output$f.csv"
+  echo "$2/$(basename "$f" ".mid").csv already processed"
+else
+  echo $f
+  echo "==========================="
+  ./melisma2003/mftext/mftext "$f" | ./melisma2003/meter/meter | ./melisma2003/harmony/harmony | grep TPCNote > "$output/$(basename "$f" .mid).csv"
 fi
+done
+# remove empty files (in case something went wrong)
+#if [ -s "output/$f.csv" ]
+#then
+#  rm -f "output$f.csv"
+#fi
