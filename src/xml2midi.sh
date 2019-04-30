c8bfe132f10ce1df554b5e9219f57e8e0a80bcf0
Fabian C. Moss <fabianmoss@gmail.com>
2019-04-30 14:59:00 +0200

    update script

--- src/xml2midi.sh
+++ src/xml2midi.sh
@@ -1,9 +1,47 @@
-#!/bin/sh
 
-mkdir output
+##!/bin/sh
 
-for f in midifiles/kp/*.xml
+# only split lines at new lines #
+IFS=$'
+'
 
-do
-  mscore -o "output/$(basename "$f" ".xml").mid" "$f"
+# set display variable (for WSL) #
+export DISPLAY=:0
+
+# i/o #
+input="$1"
+output="$2"
+
+# parsing files #
+for file in $(find $input -type f -name "*.xml"); do
+
+    # remove first directory of file path #
+    fpath="${file#"$input"}"
+
+
+    echo "$fpath - $file"
+
+    # decompose path #
+    fname=$(basename "$fpath" .xml)
+    dname=$(dirname  "$fpath")
+
+    # check if file already exists in output directory #
+    if [ -f "$2/$dname/$fname.mid" ]; then
+
+        # display message #
+        echo "already processed"
+
+    else
+
+        # create needed directories #
+        mkdir -p "$2/$dname"
+
+        # process file #
+        mscore -o "$2/$dname/$fname.mid" "$input/$dname/$fname.xml"
+
+    fi
+
+    # check if output file is empty # remove file
+    [ -s "$2/$dname/$fname.mid" ] || rm -f "$2/$dname/$fname.mid"
+
 done
